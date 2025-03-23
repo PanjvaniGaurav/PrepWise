bbf18f3fb7ee97837d79ea61ba19ad43af82040b
PanjvaniGaurav4 <panjvanigaurav4@gmail.com>
2025-03-23 22:32:12 +0530

    Setup homepage with dummy values

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" width={38} height={32} alt="Logo" />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 };
 export default RootLayout;
